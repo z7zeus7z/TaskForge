a902a1b0b37090489636e55b6602194c7a8ddc06
Zaid Sabbah <zaidsabbah89@gmail.com>
2026-09-03 22:59:37 +0300

    Allow Vercel frontend in CORS

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,11 @@ import projectRoutes from "./routes/projectRoutes.js";
 import taskRoute from "./routes/taskRoute.js";
 import errorHandler from "./middleware/errorHandler.js";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://task-forge-black.vercel.app",
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
